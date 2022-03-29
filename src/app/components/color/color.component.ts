eb3f155eb751e6e273e22483be8d32549b97bd37
harunyilmazz <harunyilmaaz@gmail.com>
2022-03-29 22:28:22 +0300

    Color division fixed

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -9,6 +9,7 @@ import { ColorService } from 'src/app/services/color.service';
 })
 export class ColorComponent implements OnInit {
   colors: Color[]=[]
+  dataLoaded= false;
   constructor(private colorService:ColorService) { }
 
   ngOnInit(): void {
@@ -18,6 +19,7 @@ export class ColorComponent implements OnInit {
   getColors(){
     this.colorService.getColors().subscribe(response=>{
       this.colors = response.data;
+      this.dataLoaded=true;
     })
   }
 
